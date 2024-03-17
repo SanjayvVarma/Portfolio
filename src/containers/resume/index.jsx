3d01a66fa86ca771768cfaecdebd39d106027ccf
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-03-18 02:45:52 +0530

    resume

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import {BsInfoCircleFill} from 'react-icons/bs'
-import PageHeaderContent from '../../component/pageHeaderContent'
+import { BsInfoCircleFill } from 'react-icons/bs'
+import PageHeaderContent from '../../component/pageHeaderContent';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css'
+import data from './utils.js'
+import './style.scss'
+import { MdWork } from 'react-icons/md'
 
 const Resume = () => {
   return (
@@ -9,7 +14,76 @@ const Resume = () => {
         headerText="My Resume"
         icon={<BsInfoCircleFill size={40} />}
       />
+      <div className='timeline'>
+        <div className='timeline__exprience'>
+          <h3 className='timeline__exprience__header-text'>Experience</h3>
+          <VerticalTimeline
+            layout={'1-column'}
+            lineColor='var(--yellow-theme-main-color)'
+          >
+            {
+              data.experience.map((item, idx) => (
+                <VerticalTimelineElement
+                  key={idx} className='timeline__exprience__vertical-timeline-element'
+                  contentStyle={{
+                    background: 'none',
+                    color: 'var(--yellow-theme-sub-text-color)',
+                    border: '1.5px solid var(--yellow-theme-main-color)'
+                  }}
+                  // date='2022-2022'
+                  icon={<MdWork />}
+                  iconStyle={{
+                    color: 'var(--yellow-theme-main-color)',
+                    background: '#181818',
+                  }}
+                >
+                  <div className='vertical-timeline-element-title-wrapper'>
+                    <h3>{item.title}</h3>
+                    <h4>{item.subTitle}</h4>
+                  </div>
+                    <p className='vertical-timeline-element-title-wrapper-description'>{item.description}</p>
+                    <p className='vertical-timeline-element-title-wrapper-description'>{item.date}</p>
 
+                </VerticalTimelineElement>
+              ))
+            }
+          </VerticalTimeline>
+        </div>
+        <div className='timeline__education'>
+          <h3 className='timeline__education__header-text'>Education</h3>
+          <VerticalTimeline
+            layout={'1-column'}
+            lineColor='var(--yellow-theme-main-color)'
+          >
+            {
+              data.education.map((item, idx) => (
+                <VerticalTimelineElement
+                  key={idx} className='timeline__exprience__vertical-timeline-element'
+                  contentStyle={{
+                    background: 'none',
+                    color: 'var(--yellow-theme-sub-text-color)',
+                    border: '1.5px solid var(--yellow-theme-main-color)'
+                  }}
+                  // date='2022-2022'
+                  icon={<MdWork />}
+                  iconStyle={{
+                    color: 'var(--yellow-theme-main-color)',
+                    background: '#181818',
+                  }}
+                >
+                  <div className='vertical-timeline-element-title-wrapper'>
+                    <h3 >{item.title}</h3>
+                    <h4 >{item.subTitle}</h4>
+                  </div>
+                    <p className='vertical-timeline-element-title-wrapper-description'>{item.description}</p>
+                    <p className='vertical-timeline-element-title-wrapper-description'>{item.date}</p>
+
+                </VerticalTimelineElement>
+              ))
+            }
+          </VerticalTimeline>
+        </div>
+      </div>
     </section>
   )
 }
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,44 @@
+const data = {
+    experience: [
+        {
+            title: "Consultant",
+            subTitle: "Miami, FL",
+            description: "Web developer, Databases, Backend developer",
+            date: "2016-17",
+        },
+        {
+            title: "Senior Consultant",
+            subTitle: "London, uk",
+            description: "Web developer, Databases, Backend developer",
+            date: "2016-17",
+        },
+        {
+            title: "Project Manager",
+            subTitle: "Bangalore, INDIA",
+            description: "Creative Direction Project Management, Team Leading",
+            date: "2016-17",
+        },
+    ],
+    education: [
+        {
+            title: "Secondary",
+            subTitle: "Govt. HSS Sarangpur",
+            description: "10th",
+            date: "2016-17",
+        },
+        {
+            title: "Higher Secondary",
+            subTitle: "Govt. HSS Sarangpur",
+            description: "12th in Science",
+            date: "2018-19",
+        },
+        {
+            title: "B.Tech",
+            subTitle: "RGPV Bhopal",
+            description: "Bachelor of technology in CSE",
+            date: "2019-23",
+        },
+    ]
+}
+
+export default data;
