cafdd3a6448fc435d548206ffa31debc312928be
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-04-07 14:17:34 +0530

    update

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # My Portfolio 
 
  ## Visit My Portfolio [click here](https://sanjayvvarma.github.io/Portfolio/)
+
+
+
+
+     "predeploy": "npm run build",
+    "deploy": "gh-pages -d build",
+
+     "gh-pages": "^6.1.1",
--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,10 +1,132 @@
-import React from "react";
+// import React, { useState } from "react";
+// import PageHeaderContent from "../../component/pageHeaderContent";
+// import { IoIosContacts } from "react-icons/io";
+// import { Animate } from "react-simple-animate";
+// import "./style.scss";
+
+// const Contact = () => {
+//   const [userData, setUserData] = useState({
+//     Name: "",
+//     Email: "",
+//     Description: "",
+//   })
+//   return (
+//     <section id="contact" className="contact">
+//       <PageHeaderContent
+//         headerText="My Contact"
+//         icon={<IoIosContacts size={40} />}
+//       />
+//       <div className="contact__content">
+//         <Animate
+//           play
+//           duration={1}
+//           delay={0}
+//           start={{
+//             transform: "translateX(-200px)",
+//           }}
+//           end={{
+//             transform: "translateX(0px)",
+//           }}
+//         >
+//           <h3 className="contact__content__header-text">Let's Talk</h3>
+//         </Animate>
+//         <Animate
+//           play
+//           duration={1}
+//           delay={0}
+//           start={{
+//             transform: "translateX(200px)",
+//           }}
+//           end={{
+//             transform: "translateX(0px)",
+//           }}
+//         >
+//           <div className="contact__content__form">
+//             <div className="contact__content__form__controlswrapper">
+//               <div>
+//                 <input
+//                   required
+//                   name="name"
+//                   className="inputName"
+//                   type={"text"}
+//                  value={}
+//                  onChange={postUser}
+//                 />
+//                 <label htmlFor="name" className="nameLabel">
+//                   Name
+//                 </label>
+//               </div>
+//               <div>
+//                 <input
+//                   required
+//                   name="email"
+//                   className="inputEmail"
+//                   type={"text"}
+//                 />
+//                 <label htmlFor="email" className="emailLabel">
+//                   Email
+//                 </label>
+//               </div>
+//               <div>
+//                 <textarea
+//                   required
+//                   name="description"
+//                   className="inputDescription"
+//                   type={"text"}
+//                   rows="5"
+//                 />
+//                 <label htmlFor="description" className="descriptionLabel">
+//                   Description
+//                 </label>
+//               </div>
+//             </div>
+//             <button>Submit</button>
+//           </div>
+//         </Animate>
+//       </div>
+//     </section>
+//   );
+// };
+// export default Contact;
+
+import React, { useState } from "react";
 import PageHeaderContent from "../../component/pageHeaderContent";
 import { IoIosContacts } from "react-icons/io";
 import { Animate } from "react-simple-animate";
 import "./style.scss";
 
 const Contact = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    description: "",
+  });
+
+  let name, value;
+  const postUserData = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUserData({ ...userData, [name]: value })
+  }
+
+  const submitData = async (e) => {
+    e.preventDefault();
+    const { name, email, description } = userData;
+    const res = await fetch('https://portfoliodatarecods-default-rtdb.firebaseio.com/userDataRecords.json',
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({name, email, description })
+      }
+    );
+    if(res){
+      alert("data stored")
+    }else{
+      alert("plz fill the data")
+    }
+  }
   return (
     <section id="contact" className="contact">
       <PageHeaderContent
@@ -37,13 +159,15 @@ const Contact = () => {
           }}
         >
           <div className="contact__content__form">
-            <div className="contact__content__form__controlswrapper">
-              <div>
+            <div method="POST" className="contact__content__form__controlswrapper">
+              <div >
                 <input
                   required
                   name="name"
                   className="inputName"
                   type={"text"}
+                  value={userData.name}
+                  onChange={postUserData}
                 />
                 <label htmlFor="name" className="nameLabel">
                   Name
@@ -55,6 +179,8 @@ const Contact = () => {
                   name="email"
                   className="inputEmail"
                   type={"text"}
+                  value={userData.email}
+                  onChange={postUserData}
                 />
                 <label htmlFor="email" className="emailLabel">
                   Email
@@ -67,13 +193,15 @@ const Contact = () => {
                   className="inputDescription"
                   type={"text"}
                   rows="5"
+                  value={userData.description}
+                  onChange={postUserData}
                 />
                 <label htmlFor="description" className="descriptionLabel">
                   Description
                 </label>
               </div>
             </div>
-            <button>Submit</button>
+            <button onClick={submitData}>Submit</button>
           </div>
         </Animate>
       </div>
